3a36f24f2164a417e84169ec03e2b286f290d80e
kris <kristopherc@codev.com>
2021-04-17 00:43:03 +0800

    Remove test module on external

--- external_addons/pos_trans_pers/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import pos_order_personel
-from . import pos_order_ext
--- external_addons/pos_trans_pers/static/src/js/mod_test.js
+++
@@ -1,9 +0,0 @@
-odoo.define('pos_trans_pers.mod_test', function (require) {
-"use strict";
-
-
-var posTest = instance.web.Class.extend({
-    say_hello: function() {
-        console.log("hello");
-    },
-});
